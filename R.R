8a8cc141ea9d606c78afedbdf2f50547c085afd6
Tremper <trempern@wdcreeodl02.cfpb.local>
2018-08-24 11:18:28 -0400

    Started R cookbook

--- R.R
+++ R.R
@@ -3,7 +3,7 @@
 ## Note that this is showing how to handle variable names
 ## passed to a function as string. df is the last argument
 ## and you have to enquo a variable first thing
-na_variable <- function(var, df = scores_sample) {
+na_variable <- function(var, df = data.frame) {
   var = enquo(var)  
   df %>% 
     filter(is.na(!!var)) %>% 
